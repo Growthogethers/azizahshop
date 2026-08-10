37efdcee977c7a5b5b00b175e31d35856efc7808
Growthogethers <growthogether1@gmail.com>
2026-08-10 14:24:20 +0700

    Delete js/utils.js

--- js/utils.js
+++
@@ -1,7 +0,0 @@
-export function debounce(func, delay = 300) {
-    let timeout;
-    return function(...args) {
-        clearTimeout(timeout);
-        timeout = setTimeout(() => func.apply(this, args), delay);
-    };
-}
